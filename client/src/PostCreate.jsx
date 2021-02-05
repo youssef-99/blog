8023e0518e2aab3def40dcad31d5646f2f21b1ed
youssef-99 <33738946+youssef-99@users.noreply.github.com>
2021-02-05 08:40:09 +0200

    attach docker

--- client/src/PostCreate.jsx
+++ client/src/PostCreate.jsx
@@ -5,7 +5,7 @@ export default () => {
     const onSubmit = async(event) => {
         event.preventDefault();
 
-        await axios.post('http://localhost:4000/posts', {
+        await axios.post('http://youssef.com/posts/create', {
             title
         });
         setTitle('');
--- posts/index.js
+++ posts/index.js
@@ -12,14 +12,14 @@ app.get('/posts', (req, res) => {
 });
 
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
     posts[id] = {
         id, title
     };
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
@@ -35,5 +35,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
+    console.log('hey skoffold');
     console.log('Listening on 4000');
 });
